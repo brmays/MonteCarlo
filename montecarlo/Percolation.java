822139493041163d123fbc76ea804acad523d974
Ben <brudellmays@gmail.com>
2018-09-05 11:53:45 +0800

    First commit

---
+++ montecarlo/Percolation.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of Percolation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Percolation {
+    
+   public Percolation(int n) {
+    
+   }
+   public void open(int row, int col)    // open site (row, col) if it is not open already
+   public boolean isOpen(int row, int col)  // is site (row, col) open?
+   public boolean isFull(int row, int col)  // is site (row, col) full?
+   public int numberOfOpenSites()       // number of open sites
+   public boolean percolates()              // does the system percolate?
+
+   public static void main(String[] args)   // test client (optional)
+}
