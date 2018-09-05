0d481b3544dc13bd930128cf5a92a26182ea61ab
Ben <brudellmays@gmail.com>
2018-09-05 12:01:25 +0800

    number 2

--- montecarlo/Percolation.java
+++ montecarlo/Percolation.java
@@ -7,14 +7,27 @@
  */
 public class Percolation {
     
-   public Percolation(int n) {
+       public Percolation(int n) {
+           //make an int[][] with all values set to n*n+1 (make a class field for this value)
+       }
+       public void open(int row, int col)  {
+           //check isOpen, if not, set to row*col, then check if surrounding
+           // are open. handle exception. if open, connect
+       }
+       public boolean isOpen(int row, int col) {
+           //reuturn the opposite of grid[][] == n*n + 1
+       }
+       public boolean isFull(int row, int col)  {
+           //not really used, maybe do a loop to see if site is connected to top row?
+       }
+       public int numberOfOpenSites() {
+           //make a class field to keep track of this
+       }
+       public boolean percolates() {
+           //double loop to check if bottom row is connected to top row
+       }
     
-   }
-   public void open(int row, int col)    // open site (row, col) if it is not open already
-   public boolean isOpen(int row, int col)  // is site (row, col) open?
-   public boolean isFull(int row, int col)  // is site (row, col) full?
-   public int numberOfOpenSites()       // number of open sites
-   public boolean percolates()              // does the system percolate?
-
-   public static void main(String[] args)   // test client (optional)
+       public static void main(String[] args) {
+       
+       }
 }
