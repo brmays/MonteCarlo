c5d1986b6ce431931b9fca235cb5115e0c588354
brmays <brudellmays@gmail.com>
2018-09-07 07:26:01 +0800

    Sept 7

--- montecarlo/Percolation.java
+++ montecarlo/Percolation.java
@@ -9,47 +9,115 @@ import edu.princeton.cs.algs4.CollisionSystem;
 import edu.princeton.cs.algs4.Particle;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.util.Random;
 
 public class Percolation {
-       public int[] grid;
+       public boolean[] grid;
        public int numOpen;
-       public int closedValue;
+       public WeightedQuickUnionUF uf;
+       public int max; 
+       public int virtExit;
        
        public Percolation(int n) {
-           closedValue = n * n + 1; //this value signifies that a site hasn't been opened yet
-           grid = new int[n*n];
-           for (int i = 0; i < n; i++) {
-               grid[i] = i +1;
-            }
+           max = n;
+           virtExit = n*n+2;
+           grid = new boolean[virtExit];
+           uf = new WeightedQuickUnionUF(virtExit);
+           virtExit--;
+           numOpen = 0;
+           for (int i = 1; i <= n; i++) { //connect the top row to the virtual source (0)
+               uf.union(0, i);
            }
+           for (int i = virtExit - (n+1); i < virtExit; i++) { //connect the bottom row to the virtual exit (0)
+               uf.union(virtExit, i);
+           }
+           grid[0]=true;
+           grid[virtExit]=true;
        }
        
        public void open(int row, int col)  {
-           //check isOpen, if not, set to row*col, then check if surrounding
-           // are open. handle exception. if open, connect
+           int curSite = row*col;
+           if(!isOpen(row, col)) {
+               grid[curSite]=true;
+               numOpen++;
+               if(row > 1 && isOpen(row-1, col)) { //look up
+                   uf.union(curSite, (row-1)*col);
+               }
+               if(row < max && isOpen(row+1, col)) { //look down
+                   uf.union(curSite, (row+1)*col);
+               }
+               if(col > 1 && isOpen(row, col-1)) { //look left
+                   uf.union(row*(col-1), curSite);
+               }
+               if(col < max && isOpen(row, col+1)) { //look right
+                   uf.union(row*(col+1), curSite);
+               }
+           }
        }
        
        public boolean isOpen(int row, int col) {
-           return grid[row][col] != closedValue;
+           return grid[row*col];
        }
        
        public boolean isFull(int row, int col)  {
-           for (int i = 0; i < grid.length; i++) {
-               if (connected(grid[row][col], grid[0][i])) {
-                   return true;
-               }
-           }
-           return false;
+           return uf.connected(0, (row*col));
        }
        
        public int numberOfOpenSites() {
            return numOpen;
        }
+       
        public boolean percolates() {
-           //double loop to check if bottom row is connected to top row
+           return uf.connected(0, virtExit);
        }
     
-       public static void main(String[] args) {
-       
+       public void tester() {
+           Random rand = new Random();
+           int n = 5;
+           boolean readyToPercolate = true;
+           
+           while (readyToPercolate) {
+               int changeTrack = numOpen;
+               int col = rand.nextInt(n) + 1;
+               int row = rand.nextInt(n) + 1;
+               open(row, col);
+               if (changeTrack != numOpen) {
+                   if (percolates()) {
+                       break;
+                   }
+                   
+               }
+               for (int i = 1; i <=n*n; i++) {
+               if (grid[i] && !uf.connected(0, i)) {
+                   System.out.print(" O ");
+               } else if (grid[i] && uf.connected(0, i)) {
+                   System.out.print(" 0 ");
+               } else {
+                   System.out.print(" - ");
+               }
+               if (i%n==0) {
+                   System.out.print("\n");
+               }
+           }
+           
+           System.out.println("___________________");
+           }
+           
+           System.out.println(numOpen + " sites were open when it started percolating. This is about " + ((float) numOpen/(n*n)));
+           int row = 1;
+           int col = 1;
+           for (int i = 1; i <=n*n; i++) {
+               if (grid[i] && !uf.connected(0, i)) {
+                   System.out.print(" O ");
+               } else if (grid[i] && uf.connected(0, i)) {
+                   System.out.print(" 0 ");
+               } else {
+                   System.out.print(" - ");
+               }
+               if (i%n==0) {
+                   System.out.print("\n");
+               }
+           }
        }
-}
+    }
+
